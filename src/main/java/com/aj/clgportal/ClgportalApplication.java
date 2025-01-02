804c21306b108e1efc5c9d9279288aca6fa47027
ajaikamble10 <ajaysuhaskamble@gmail.com>
2025-01-02 11:07:39 +0530

    initial commit.

---
+++ src/main/java/com/aj/clgportal/ClgportalApplication.java
@@ -0,0 +1,20 @@
+package com.aj.clgportal;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ClgportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClgportalApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper ModelMapper() {
+		return new ModelMapper();
+	}
+
+}
---
+++ src/test/java/com/aj/clgportal/ClgportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.clgportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClgportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
