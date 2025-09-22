9562492a56c5dd1d1258cafd5eb41d0571d1bc2c
theajdev <theajdev@gmail.com>
2025-09-22 09:22:52 +0530

    Update TeacherServiceImpl.java
    import organizied.

--- src/main/java/com/aj/clgportal/serviceImpl/TeacherServiceImpl.java
+++ src/main/java/com/aj/clgportal/serviceImpl/TeacherServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.aj.clgportal.dto.RoleDto;
 import com.aj.clgportal.dto.TeacherDto;
 import com.aj.clgportal.entity.Department;
 import com.aj.clgportal.entity.Role;
