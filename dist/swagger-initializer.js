64d9c066d3ce3add61d64d9f385fd01f9ab11e8e
theajdev <theajdev@gmail.com>
2026-09-13 22:47:13 +0530

    modified files.

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,6 +1,9 @@
 window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://api.example.com/v3/api-docs",
+    url:"v3/api-docs",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -12,4 +15,6 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
+
+  //</editor-fold>
 };
