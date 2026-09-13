2e67ea17addf694d360eb8ed763c09838d3a7bfa
theajdev <theajdev@gmail.com>
2026-09-13 21:32:40 +0530

    modified swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:8080/v3/api-docs",,
+    url: "http://localhost:2025/v3/api-docs",,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
